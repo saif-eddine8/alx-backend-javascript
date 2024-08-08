72215080b7d37d47ece46f3a30ff2f1f73cfd923
saif-eddine <saif.elmoakouil@gmail.com>
2024-08-08 13:57:38 +0100

    advanced files

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-  for (const item of reportWithIterator) {
-    employees.push(item);
+  let output = '';
+
+  for (const [index, item] of Object.entries(reportWithIterator)) {
+    output += `${item}`;
+
+    if (parseInt(index) !== reportWithIterator.length - 1) {
+      output += ' | ';
+    }
   }
-  return employees.join(' | ');
+
+  return output;
 }
