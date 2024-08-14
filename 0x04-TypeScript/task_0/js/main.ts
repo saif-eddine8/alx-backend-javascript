a6c245f949491e303cf25014186a8d425b944434
saif-eddine <saif.elmoakouil@gmail.com>
2024-08-14 23:47:20 +0100

    all files

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,37 @@
+// Define the Student interface
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+// Create two student objects
+const firstStudent: Student = {
+	firstName: "Obed",
+	lastName: "Ehoneah",
+	age: 97,
+	location: "Accra"
+}
+
+const secondStudent: Student = {
+	firstName: "Frank",
+	lastName: "Ehoneah",
+	age: 79,
+	location: "Kumasi"
+}
+
+// Create an array of students
+const studentsList = [firstStudent, secondStudent];
+
+const table = document.getElementById('studentTable') as HTMLTableElement;
+
+// Create a new row for each student and append it to the table
+studentsList.forEach((student) => {
+	const row = table.insertRow();
+	const firstNameCell = row.insertCell(0);
+	const locationCell = row.insertCell(1);
+
+	firstNameCell.innerHTML = student.firstName;
+	locationCell.innerHTML = student.location;
+})
